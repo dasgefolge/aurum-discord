b28b09f809577fcc3b55ac84a75fd5bf6f23ef11
Henrik <dev@infectedbytes.com>
2018-09-12 17:42:54 +0200

    initial commit

---
+++ src/main/java/com/infectedbytes/discordbot/DiscordBot.java
@@ -0,0 +1,126 @@
+package com.infectedbytes.discordbot;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.minecraft.entity.player.EntityPlayerMP;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.ServerChatEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod(modid = DiscordBot.MOD_ID, version = DiscordBot.VERSION, acceptableRemoteVersions = "*")
+public class DiscordBot {
+    static final String MOD_ID = "discordbot";
+    static final String VERSION = "1.0";
+
+    private Configuration config;
+    private JDA jda;
+    private TextChannel chatChannel;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        config = new Configuration(e.getSuggestedConfigurationFile());
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        setupDiscord();
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new MinecraftForwarder());
+        FMLCommonHandler.instance().bus().register(new LoginHandler());
+    }
+
+    @EventHandler
+    public void serverStopping(FMLServerStoppingEvent event) {
+        shutdownDiscord();
+    }
+
+    private void setupDiscord(){
+        try {
+            config.load();
+            Settings.read(config);
+            jda = new JDABuilder(AccountType.BOT).setToken(Settings.token).buildBlocking();
+            jda.addEventListener(new DiscordForwarder());
+            chatChannel = jda.getTextChannelById(Settings.channelId);
+            updatePlayerList(null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            shutdownDiscord();
+        } finally {
+            if (config.hasChanged()) config.save();
+        }
+    }
+
+    private void shutdownDiscord() {
+        if(jda != null) {
+            jda.shutdownNow();
+            jda = null;
+            chatChannel = null;
+        }
+    }
+
+    private void updatePlayerList(List<EntityPlayerMP> players) {
+        if (chatChannel == null) return;
+        String title = Settings.Messages.getUsers(players);
+        try {
+            chatChannel.getManager().setTopic(title).queue();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void sendGlobal(String msg) {
+        for (Object entity : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
+            ((EntityPlayerMP) entity).addChatMessage(new ChatComponentText(msg));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public class LoginHandler {
+        @SubscribeEvent
+        public void login(PlayerEvent.PlayerLoggedInEvent e) {
+            updatePlayerList((List<EntityPlayerMP>) MinecraftServer.getServer().getConfigurationManager().playerEntityList);
+        }
+
+        @SubscribeEvent
+        public void logout(PlayerEvent.PlayerLoggedOutEvent e) {
+            List<EntityPlayerMP> players = new ArrayList<EntityPlayerMP>(MinecraftServer.getServer().getConfigurationManager().playerEntityList);
+            //noinspection SuspiciousMethodCalls
+            players.remove(e.player);
+            updatePlayerList(players);
+        }
+    }
+
+    public class MinecraftForwarder {
+        @SubscribeEvent(priority = EventPriority.LOWEST)
+        public void onChatEvent(ServerChatEvent e) {
+            if (chatChannel == null) return;
+            String msg = Settings.Messages.formatFromMinecraft(e.username, e.message);
+            if (msg != null) chatChannel.sendMessage(msg).queue();
+        }
+    }
+
+    public class DiscordForwarder extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(MessageReceivedEvent event) {
+            if (event.getAuthor().isBot()) return;
+            String msg = Settings.Messages.formatFromDiscord(event.getAuthor().getName(), event.getMessage().getContentDisplay());
+            if (msg != null) sendGlobal(msg);
+        }
+    }
+}
---
+++ src/main/java/com/infectedbytes/discordbot/Settings.java
@@ -0,0 +1,66 @@
+package com.infectedbytes.discordbot;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.config.Configuration;
+
+import java.util.List;
+
+final class Settings {
+    static String token;
+    static long channelId;
+
+    static class Messages {
+        static String empty;
+        static String many;
+        static String normal;
+        static String fromMinecraft;
+        static String fromDiscord;
+
+        static String getUsers(List<EntityPlayerMP> users) {
+            if (users == null || users.size() == 0)
+                return empty;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < users.size(); i++) {
+                if (i > 0) sb.append(", ");
+                sb.append(users.get(i).getDisplayName());
+            }
+
+            try {
+                return sb.length() > 1024
+                        ? String.format(many, users.size())
+                        : String.format(normal, sb.toString());
+            } catch (Exception ex) {
+                return "Error";
+            }
+        }
+
+        static String formatFromMinecraft(String user, String msg) {
+            try {
+                return String.format(fromMinecraft, user, msg);
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+
+        static String formatFromDiscord(String user, String msg) {
+            try {
+                return String.format(fromDiscord, user, msg);
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+    }
+
+    static void read(Configuration config){
+        token = config.get("general", "token", "").getString();
+        channelId = Long.parseLong(config.get("general", "channelId", "0").getString());
+
+        Messages.empty = config.get("messages", "empty", "Server is abandoned").getString();
+        Messages.many = config.get("messages", "many", "%d users are currently online").getString();
+        Messages.normal = config.get("messages", "normal", "Online: %s").getString();
+
+        Messages.fromMinecraft = config.get("messages", "fromMinecraft", "`%s`: %s").getString();
+        Messages.fromDiscord = config.get("messages", "fromDiscord", "[§2Discord§r] %s: %s").getString();
+    }
+}
